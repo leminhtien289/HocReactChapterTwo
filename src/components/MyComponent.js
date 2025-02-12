7c58cd1255f87f4788c2b01862cfa9a948b37705
lolicon <leminhtien2892004@gmail.com>
2025-02-12 15:23:08 +0700

    props

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,14 +3,21 @@
 
 import React from 'react';
 import UserInfor from './UserInfor';
+import DisplayInfor from './DisplayInfor';
 
 class MyComponent extends React.Component {
 
     //JSX
     render() {
+        const myInfor = ['a', 'b', 'c'];
         return (
             <div>
                 <UserInfor />
+                <br></br>
+                <br></br>
+                <DisplayInfor name="Nahihi" age="21" />
+                <hr></hr>
+                <DisplayInfor name="Nahihi" age={26} myInfor={myInfor} />
             </div >
         )
     }
