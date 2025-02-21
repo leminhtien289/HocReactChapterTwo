001160f21142067959a58df43c987d9a655ebb6f
lolicon <leminhtien2892004@gmail.com>
2025-02-21 11:03:27 +0700

    CSS with react

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './DisplayInfor.scss'
 
 class DisplayInfor extends React.Component {
     state = {
@@ -19,7 +20,7 @@ class DisplayInfor extends React.Component {
         // console.table(listUsers);
 
         return (
-            <div>
+            <div className='display-infor-container'>
                 <div>
                     <span onClick={() => { this.handleShowHide() }}>
                         {this.state.isShowListUser === true ? "Hide list user:" : "Show list user:"}
@@ -31,7 +32,7 @@ class DisplayInfor extends React.Component {
 
                             return (
                                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                                    <div>My name's {user.name}</div>
+                                    <div style={{ color: 'yellow', paddingTop: '50px' }}>My name's {user.name}</div>
                                     <div>My age's {user.age}</div>
                                     <hr></hr>
                                 </div>
