f7805a93792b7608ca7195ac8af8ec0e91e26322
lolicon <leminhtien2892004@gmail.com>
2025-02-21 11:10:49 +0700

    img with react

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './DisplayInfor.scss'
+import logo from './../logo.svg';
 
 class DisplayInfor extends React.Component {
     state = {
@@ -21,6 +22,7 @@ class DisplayInfor extends React.Component {
 
         return (
             <div className='display-infor-container'>
+                <img src={logo} />
                 <div>
                     <span onClick={() => { this.handleShowHide() }}>
                         {this.state.isShowListUser === true ? "Hide list user:" : "Show list user:"}
@@ -32,7 +34,7 @@ class DisplayInfor extends React.Component {
 
                             return (
                                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                                    <div style={{ color: 'yellow', paddingTop: '50px' }}>My name's {user.name}</div>
+                                    <div>My name's {user.name}</div>
                                     <div>My age's {user.age}</div>
                                     <hr></hr>
                                 </div>
