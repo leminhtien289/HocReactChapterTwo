8be076dd6e5a70cd0c87d3c12d1e2cfd70df382b
lolicon <leminhtien2892004@gmail.com>
2025-02-28 22:05:08 +0700

    useEffect

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,61 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './DisplayInfor.scss'
-import logo from './../logo.svg';
+// import logo from './../logo.svg';
 
 //stateless vs statefuls
-// class DisplayInfor extends React.Component {
-//     render() {
-//         console.log(">>> call me render");
-
-//         //props: properties  
-//         // destructuring array/object
-//         const { listUsers } = this.props;//object
-
-//         // template + logic js
-//         return (
-//             <div className='display-infor-container'>
-//                 {true &&
-//                     <>
-//                         {listUsers.map((user) => {
-
-//                             return (
-//                                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-//                                     <div>My name's {user.name}</div>
-//                                     <div>My age's {user.age}</div>
-//                                     <div>
-//                                         <button onClick={() => { this.props.handleDeleteUser(user.id) }}>
-//                                             Delete
-//                                         </button>
-//                                     </div>
-//                                     <hr></hr>
-//                                 </div>
-//                             )
-
-
-//                         })}
-
-//                     </>
-//                 }
-//             </div>
-//         );
-//     }
-// }
 
 const DisplayInfor = (props) => {
     const { listUsers } = props;//object
 
     const [isShowHideListUser, setShowHideListUser] = useState(true);
 
-    // this.state = {
-    //     isShowHideListUser: true
-    // }
-
     const handleShowHideListUser = () => {
-        // this.setState = {
-        //     isShowHideListUser: true
-        // }
         setShowHideListUser(!isShowHideListUser);
     };
+
+    useEffect(() => {
+        if (listUsers.length === 0) {
+            alert("You deleted all")
+        }
+        console.log(">>> call me useEffect")
+    }, [listUsers]);
 
     return (
         <div className='display-infor-container'>
