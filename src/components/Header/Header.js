bf9cba2c341787f1f972c7217a86a32ee39b6587
lolicon <leminhtien2892004@gmail.com>
2025-03-01 14:14:44 +0700

    Index Routers

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,8 @@ const Header = () => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">Hỏi Dân IT</Navbar.Brand>
+                {/* <Navbar.Brand href="#home">Hỏi Dân IT</Navbar.Brand> */}
+                <NavLink to='/' className='navbar-brand'>Hỏi dânIT</NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
@@ -18,13 +19,13 @@ const Header = () => {
                     </Nav>
                     <Nav>
                         <NavDropdown title="Settings" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">
+                            <NavDropdown.Item>
                                 Log in
                             </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
+                            <NavDropdown.Item>
                                 Log out
                             </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">
+                            <NavDropdown.Item>
                                 Profile
                             </NavDropdown.Item>
                         </NavDropdown>
