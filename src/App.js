e537f0984c1307210a13d5c88609ed11615051a2
lolicon <leminhtien2892004@gmail.com>
2025-03-01 11:43:46 +0700

    setup bootstrap + router dom

--- src/App.js
+++ src/App.js
@@ -1,38 +1,12 @@
-import logo from './logo.svg';
 import './App.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import MyComponent from './components/MyComponent';
-import React from 'react';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='app-containers'>
-        Hello world Nahihi
-        <MyComponent></MyComponent>
-      </div>
-    );
-  }
+const App = () => {
+
+  return (
+    <div className="app-container">
+      Hello world
+    </div>
+  );
 }
-
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Hello world Nahihi
-//         </p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
